a05935398185deac17cbf3abed915786eab0dbdf
Qi Wang <mpiglet@outlook.com>
2024-03-11 20:43:12 +0800

    more

--- src/bin/nowcoder-04-e.rs
+++ src/bin/nowcoder-04-e.rs
@@ -1,3 +1,4 @@
+const DIRS: [(i64, i64); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 fn main() {
     let mut nums = String::new();
     std::io::stdin().read_line(&mut nums);
@@ -9,18 +10,44 @@ fn main() {
     let row = nums[0];
     let col = nums[1];
     let mut maze = vec![vec!['0'; col]; row];
-    let mut is_a = true;
-    if row >= col {
-    for r in 0..row {
-        let rev = r % 2 == 0;
-        let its = if rev { (r..row) } else { (r..row).rev() };
-        for idx in its {
-            maze[r][idx] = if is_a { 'a' } else { 'b' };
-            is_a = !is_a;
+    let begs = [['a', 'b', 'b'], ['a', 'c', 'c'], ['b', 'c', 'a']];
+    for r in 0..begs.len() {
+        for c in 0..begs[0].len() {
+            maze[r][c] = begs[r][c];
         }
     }
-    } else {
 
+    let choices = vec!['a', 'b', 'c', 'd'];
+    for r in 0..row {
+        for c in 0..col {
+            if maze[r][c] == '0' {
+                let mut found = vec![false; choices.len()];
+                for dir in &DIRS {
+                    let nr = r as i64 + dir.0;
+                    let nc = c as i64 + dir.1;
+                    if nr >= 0 && nc >= 0 && nr < row as i64 && nc < col as i64 {
+                        let ch = maze[nr as usize][nc as usize];
+                        if ch == '0' {
+                            continue;
+                        }
+                        let idx = ch as usize - 'a' as usize;
+                        found[idx] = true;
+                    }
+                }
+                let idx = found
+                    .iter()
+                    .enumerate()
+                    .find(|(idx, v)| **v == false)
+                    .unwrap()
+                    .0;
+                maze[r][c] = ('a' as u8 + idx as u8) as char;
+            }
+        }
     }
-    for r in 
+    for r in 0..row {
+        for c in 0..col {
+            print!("{}", maze[r][c]);
+        }
+        println!("");
+    }
 }
