7193c14a130ae0ded9307a67db8b6043afbcac4a
Qi Wang <mpiglet@outlook.com>
2024-03-11 08:43:53 +0800

    more

--- src/bin/nowcoder-04-d.rs
+++ src/bin/nowcoder-04-d.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 const DIRS: [(i64, i64); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 fn main() {
     let mut nums = String::new();
@@ -17,63 +18,38 @@ fn main() {
         let curr_row = curr_row.trim().chars().collect::<Vec<_>>();
         maze.push(curr_row);
     }
-    let mut visitd = vec![vec![i32::MAX; col]; row];
-    visitd[0][0] = 0;
-    if let Some(step) = dfs(&maze, 0, 0, &mut visitd, 0) {
-        println!("{}", step);
-    } else {
-        println!("-1");
-    }
-}
+    let mut queue = VecDeque::new();
+    let mut visited = vec![vec![false; col]; row];
+    visited[0][0] = true;
+    queue.push_back((0, 0));
 
-fn dfs(
-    maze: &Vec<Vec<char>>,
-    crow: usize,
-    ccol: usize,
-    visitd: &mut Vec<Vec<i32>>,
-    steps: usize,
-) -> Option<i32> {
-    if crow == maze.len() - 1 && ccol == maze[0].len() - 1 {
-        return Some(steps as i32);
-    }
-    let cur = maze[crow][ccol];
-    let mut ret = i32::MAX;
-    for dir in &DIRS {
-        let nrow = crow as i64 + dir.0;
-        let ncol = ccol as i64 + dir.1;
-        if nrow >= 0
-            && nrow < maze.len() as i64
-            && ncol >= 0
-            && ncol < maze[0].len() as i64
-            && maze[nrow as usize][ncol as usize] != cur
-        {
-            if visitd[nrow as usize][ncol as usize] > steps as i32 + 1 {
-                visitd[nrow as usize][ncol as usize] = steps as i32 + 1;
+    let mut curr_steps = 0;
+    while !queue.is_empty() {
+        let sz = queue.len();
+        for _ in 0..sz {
+            let (crow, ccol) = queue.pop_front().unwrap();
+            let cur = maze[crow][ccol];
+            if crow == row - 1 && ccol == col - 1 {
+                println!("{}", curr_steps);
+                return;
             }
-        }
-    }
-    for dir in &DIRS {
-        let nrow = crow as i64 + dir.0;
-        let ncol = ccol as i64 + dir.1;
-        if nrow >= 0
-            && nrow < maze.len() as i64
-            && ncol >= 0
-            && ncol < maze[0].len() as i64
-            && maze[nrow as usize][ncol as usize] != cur
-        {
-            if visitd[nrow as usize][ncol as usize] == steps as i32 + 1 {
-                if let Some(v) = dfs(maze, nrow as usize, ncol as usize, visitd, steps + 1) {
-                    ret = ret.min(v);
-                    if ret as usize <= steps + maze.len() - crow + maze[0].len() - ccol - 1 {
-                        return Some(ret);
-                    }
+            for dir in &DIRS {
+                let nrow = crow as i64 + dir.0;
+                let ncol = ccol as i64 + dir.1;
+                if nrow >= 0
+                    && nrow < maze.len() as i64
+                    && ncol >= 0
+                    && ncol < maze[0].len() as i64
+                    && maze[nrow as usize][ncol as usize] != cur
+                    && !visited[nrow as usize][ncol as usize]
+                {
+                    visited[nrow][ncol] = true;
+                    queue.push_back((nrow as usize, ncol as usize));
                 }
             }
         }
+        curr_steps += 1;
     }
-    if ret == i32::MAX {
-        None
-    } else {
-        Some(ret)
-    }
+
+    println!("-1");
 }
