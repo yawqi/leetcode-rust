440d90fba8cbbb735ae7c3e9b7391124f9d15463
Qi Wang <mpiglet@outlook.com>
2024-03-19 22:45:22 +0800

    more

--- src/bin/nowcoder-06-f.rs
+++ src/bin/nowcoder-06-f.rs
@@ -18,11 +18,28 @@ fn main() {
         let nums = s
             .trim()
             .split(' ')
-            .map(|n| n.parse::<usize>().unwrap())
+            .map(|n| n.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
+
+        let mut dp = vec![200001; 128];
+        dp[127] = 0;
+
+        for v in nums {
+            let mut ndp = dp.clone();
+            for pv in 1..=127 {
+                ndp[(v & pv) as usize] = std::cmp::min(dp[pv as usize] + 1, ndp[(v & pv) as usize]);
+            }
+            dp = ndp;
+        }
+        if dp[0] >= 200001 {
+            dp[0] = -1;
+        } else {
+            dp[0] = n as i32 - dp[0];
+        }
+        ans.push(dp[0]);
     }
 
     for a in ans {
-        println!("{}", ans);
+        println!("{}", a);
     }
 }
